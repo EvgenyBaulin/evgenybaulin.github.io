4cffcbeb04496213d7f4f15ac7127d1d4de703cd
evgenybaulin <e.baulin@icloud.com>
2024-11-05 15:53:29 +0300

    Test run server on GitHub

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/evgenybaulin.github.io/', // Добавьте эту строку для указания пути
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
 })
