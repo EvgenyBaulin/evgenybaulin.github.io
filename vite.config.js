6fa87bb10dcf76b00d49a42e3645d3916222244b
evgenybaulin <e.baulin@icloud.com>
2024-11-07 22:19:34 +0300

    Working website

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite';
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
 })
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/my-project/'
+        : '/'
+}
